33fc04a063b569ac57983b332168a4d18e1a10fa
zakitono <coppercraft1313@gmail.com>
2020-12-20 00:36:17 +0900

    index,new,createアクションを定義

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,8 +2,24 @@ class PostsController < ApplicationController
   before_action :sign_in_required, only: [:show]
   
   def index
+    @posts = Post.all
   end
 
   def show
   end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.save
+    redirect_to action: 'index'
+  end
+  
+  private
+    def post_params
+      params.require(:post).permit(:title, :content)
+    end
 end
