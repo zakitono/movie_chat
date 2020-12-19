6d17d76ded0a81d0e8f905770efa41b8833531e9
zakitono <coppercraft1313@gmail.com>
2020-12-19 21:20:10 +0900

    投稿機能のルーティングの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'posts#index'
   get 'posts/show'
+  resources :posts
   devise_for :users
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
