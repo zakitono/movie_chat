2c6dd62a1e05a3b2775384fc13da89947ce34354
zakitono <coppercraft1313@gmail.com>
2020-12-19 21:13:57 +0900

    Postモデル、postsテーブルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_14_114157) do
+ActiveRecord::Schema.define(version: 2020_12_19_120843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.text "title"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
