882528de6df2394c14861e7fe97383ce77b3a386
zakitono <coppercraft1313@gmail.com>
2020-12-20 23:21:51 +0900

    user_idをhiddenで送る設定

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -23,6 +23,6 @@ class PostsController < ApplicationController
   
   private
     def post_params
-      params.require(:post).permit(:title, :content)
+      params.require(:post).permit(:title, :content, :user_id)
     end
 end
