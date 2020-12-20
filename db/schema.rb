9c650087713aa7bade79a5b50308547e75fef51f
zakitono <coppercraft1313@gmail.com>
2020-12-20 23:18:16 +0900

    Postモデルにuser_idカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_19_120843) do
+ActiveRecord::Schema.define(version: 2020_12_20_140246) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2020_12_19_120843) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
