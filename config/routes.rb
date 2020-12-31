064015c026fc5bf71f20b262c7ec6f8f860fb6a7
zakitono <coppercraft1313@gmail.com>
2020-12-31 17:10:25 +0900

    ルーティングの追加（rooms）

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   resources :posts
   devise_for :users, controllers: {registrations: 'users/registrations'}
   resources :users, :only => [:show]
+  resources :rooms
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
