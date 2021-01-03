5d9bbf42be83a64aeac994b058a2b47811c2fb83
zakitono <coppercraft1313@gmail.com>
2021-01-03 15:36:47 +0900

    usersコントローラーの編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,23 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+      if user_signed_in?
+        @currentUserEntry = Entry.where(user_id: current_user.id)
+        @userEntry = Entry.where(user_id: @user.id)
+        unless @user.id == current_user.id
+          @currentUserEntry.each do |cu|
+            @userEntry.each do |u|
+              if cu.room_id == u.room_id then
+                @isRoom = true
+                @roomId = cu.room_id
+              end
+            end
+          end
+          unless @isRoom
+            @room = Room.new
+            @entry = Entry.new
+          end
+        end
+    end
   end
 end
