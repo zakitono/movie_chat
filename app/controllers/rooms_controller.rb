fb69978ef9d734f06692f602305e6136618f92ed
zakitono <coppercraft1313@gmail.com>
2021-01-03 15:42:11 +0900

    roomsコントローラーの作成

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,2 +1,23 @@
 class RoomsController < ApplicationController
+    before_action :authenticate_user!
+
+    def show
+        @room = Room.find(params[:id])
+        #ルームが作成されているかどうか
+        if Entry.where(:user_id => current_user.id, :room_id => @room.id).present?
+          @messages = @room.messages
+          @entries = @room.entries
+        else
+          redirect_back(fallback_location: root_path)
+        end
+    end
+    
+    def create
+        @room = Room.create(:name => "DM")
+        #entryにログインユーザーを作成
+        @entry1 = Entry.create(:room_id => @room.id, :user_id => current_user.id)
+        #entryにparamsユーザーを作成
+        @entry2 = Entry.create(params.require(:entry).permit(:user_id, :room_id).merge(:room_id => @room.id))
+        redirect_to room_path(@room.id)
+    end
 end
