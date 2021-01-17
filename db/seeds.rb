e5a87135faf6c99f212512b7dd9b8cafb8992b3b
zakitono <coppercraft1313@gmail.com>
2021-01-18 01:25:54 +0900

    [追加]seedファイルの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,34 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+User.create!(
+    [
+      {
+        email: 'rope@movie.com',
+        password: 'rope2222',
+        username: 'ロペ',
+        movie_a: '鬼滅の刃',
+        movie_b: 'えんとつ町のプペル',
+        movie_c: 'トイストーリー',
+        confirmed_at: Time.now
+      },
+      {
+        email: 'akira@movie.com',
+        password: 'akira2222',
+        username: 'アキラ先輩',
+        movie_a: 'ゴッドファーザー',
+        movie_b: 'グッドフェローズ',
+        movie_c: 'ヒート',
+        confirmed_at: Time.now
+      },
+      {
+        email: 'nori@movie.com',
+        password: 'nori2222',
+        username: 'ノリユキ',
+        movie_a: 'ガタカ',
+        movie_b: 'ROMA',
+        movie_c: 'インセプション',
+        confirmed_at: Time.now
+      }
+    ]
+  )
