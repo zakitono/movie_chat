9b9d4a1de738ed5129d63b67ceebf2c9015d3471
zakitono <coppercraft1313@gmail.com>
2021-01-20 13:07:56 +0900

    確認メールの設定変更、チャット機能が非同期処理するように修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,7 +44,7 @@ Rails.application.configure do
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.allowed_request_origins = [ /http:\/\/.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -64,6 +64,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "movie_chat_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { :host => 'https://movies-chat.herokuapp.com/' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
