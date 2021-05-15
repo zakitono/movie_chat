e67b9e87eeb37b894da13951e0eaaaa14c09dd41
zakitono <coppercraft1313@gmail.com>
2021-05-15 16:24:08 +0900

    アソシエーションの定義

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,7 @@
 class Post < ApplicationRecord
     belongs_to :user
+    has_many :likes, dependent: :destroy
+    has_many :users, through: :likes
     validates :title, presence: true, length: { maximum: 50 }
     validates :content, presence: true, length: { maximum: 140 }
 end
